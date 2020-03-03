ea71c72dd98314352188e5679abb607c4167516b
Bhanu <pbhanu.1994@gmail.com>
2020-03-03 13:34:42 +1000

    Added the Pagination

--- server/src/schema.js
+++ server/src/schema.js
@@ -32,9 +32,24 @@ const typeDefs = gql`
     }
 
     type Query {
-        launches: [Launch]!
+        launches( # replace the current launches query with this one.
+            """
+            The number of results to show. Must be >= 1. Default = 20
+            """
+            pageSize: Int
+            """
+            If you add a cursor here, it will only return results _after_ this cursor
+            """
+            after: String
+          ): LaunchConnection!
         launch(id: ID!): Launch
         me: User
+    }
+
+    type LaunchConnection { # add this below the Query type as an additional type. 
+        cursor: String!
+        hasMore: Boolean!
+        launches: [Launch]!
     }
 
     type Mutation {
